5aa18f7634e23d9a791b6d5f686bdf9928f26d56
SuhailiJunaidi <a173588@siswa.ukm.edu.my>
2022-07-06 19:17:06 +0800

    Updates

---
+++ precache-manifest.9c99667483b8845558490750bed3f05c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21b9a152892ff3acdbd7a7bb26d2ca04",
+    "url": "/FYP/index.html"
+  },
+  {
+    "revision": "ad91e22f81d94b8e054b",
+    "url": "/FYP/static/css/2.18fb2ca0.chunk.css"
+  },
+  {
+    "revision": "a42ac9b1b844d8cd64ff",
+    "url": "/FYP/static/css/main.fa5b3062.chunk.css"
+  },
+  {
+    "revision": "ad91e22f81d94b8e054b",
+    "url": "/FYP/static/js/2.f343a1f0.chunk.js"
+  },
+  {
+    "revision": "6ad6b4e8816ec58fe488c5d7be2cf06a",
+    "url": "/FYP/static/js/2.f343a1f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a42ac9b1b844d8cd64ff",
+    "url": "/FYP/static/js/main.89e8c826.chunk.js"
+  },
+  {
+    "revision": "4221f350825a974143b9",
+    "url": "/FYP/static/js/runtime-main.64e698c7.js"
+  }
+]);
